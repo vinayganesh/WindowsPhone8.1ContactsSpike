f24b78fadbc1e7440dbda8a9052c3415ed49a09a
vinayganeshh <vinayganeshh@gmail.com>
2014-08-02 11:46:04 -0700

    Initial commit

---
+++ ContactsPlayNiceSpike/GetContacts.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.UserData;
+
+namespace ContactsPlayNiceSpike
+{
+    public partial class GetContacts : PhoneApplicationPage
+    {
+        public GetContacts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetContacts_Click(object sender, RoutedEventArgs e)
+        {
+            Contacts cons = new Contacts();
+
+            //Identify the method that runs after the asynchronous search completes.
+            cons.SearchCompleted += new EventHandler<ContactsSearchEventArgs>(Contacts_SearchCompleted);
+
+            //Start the asynchronous search.
+            cons.SearchAsync(String.Empty, FilterKind.None, "Contacts Test #1");
+        }
+
+        private void Contacts_SearchCompleted(object sender, ContactsSearchEventArgs e)
+        {
+            try
+            {
+                //Bind the results to the user interface.
+                ContactResultsData.DataContext = e.Results;
+            }
+            catch (System.Exception)
+            {
+                //No results
+            }
+
+            if (ContactResultsData.Items.Any())
+            {
+                ContactResultsLabel.Text = "results";
+            }
+            else
+            {
+                ContactResultsLabel.Text = "no results";
+            }
+
+        }
+
+        public void enumerateContacts()
+        {
+           // System.Text.StringBuilder sb = new System.Text.StringBuilder();
+           //
+           // foreach (Contact con in e.Results)
+           // {
+           //     sb.AppendLine(con.DisplayName);
+           // }
+           //
+           // MessageBox.Show(sb.ToString());
+        }
+    }
+}
---
+++ ContactsPlayNiceSpike/GetContactsWithImages.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.UserData;
+
+namespace ContactsPlayNiceSpike
+{
+    public partial class GetContactsWithImages : PhoneApplicationPage
+    {
+        public GetContactsWithImages()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonManyPictures_Click(object sender, RoutedEventArgs e)
+        {
+            Contacts cons = new Contacts();
+
+            //Identify the method that runs after the asynchronous search completes.
+            cons.SearchCompleted += new EventHandler<ContactsSearchEventArgs>(Contacts_SearchCompleted_Many);
+
+            //Start the asynchronous search.
+            cons.SearchAsync(String.Empty, FilterKind.None, "Contacts Test #3 Picture");
+        }
+
+        private void Contacts_SearchCompleted_Many(object sender, ContactsSearchEventArgs e)
+        {
+            try
+            {
+                //Bind the results to the list box that displays them in the UI.
+                ContactResultsData1.DataContext = e.Results;
+            }
+            catch (System.Exception)
+            {
+                //No results
+            }
+        }
+    }
+
+    public class ContactPictureConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Contact c = value as Contact;
+            if (c == null) return null;
+
+            System.IO.Stream imageStream = c.GetPicture();
+            if (null != imageStream)
+            {
+                return Microsoft.Phone.PictureDecoder.DecodeJpeg(imageStream);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
